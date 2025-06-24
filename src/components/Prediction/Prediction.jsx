38576d28e469d4a57593004c454bd74e76bea720
Kiran5077 <145216761+Kiran5077@users.noreply.github.com>
2025-06-24 10:41:56 +0530

    Update Prediction.jsx

--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -20,9 +20,7 @@ const Prediction = () => {
 
   const handleUpload = async (e) => {
     e.preventDefault();
-    const storedEmail = localStorage.getItem("userEmail");
     if (!selectedFile) return alert("Please select a brain scan file to upload.");
-    if (!storedEmail) return alert("Please register or log in first to use the prediction feature.");
 
     const formData = new FormData();
     formData.append("file", selectedFile);
