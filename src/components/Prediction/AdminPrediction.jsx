83bd6fa343cc264b73eddba2beae776b47bfd7d9
Kiran5077 <145216761+Kiran5077@users.noreply.github.com>
2026-01-25 21:23:49 +0530

    Update API endpoints for predictions

--- src/components/Prediction/AdminPrediction.jsx
+++ src/components/Prediction/AdminPrediction.jsx
@@ -29,7 +29,7 @@ const AdminPrediction = () => {
     formData.append('email', email);
 
     try {
-      const res = await fetch('https://brain-tumourbackend.onrender.com/predict', {
+      const res = await fetch('https://brain-server-xuxb.onrender.com/predict', {
         method: 'POST',
         body: formData,
       });
@@ -48,7 +48,7 @@ const AdminPrediction = () => {
   useEffect(() => {
     const fetchPredictions = async () => {
       try {
-        const response = await fetch("https://brain-tumourbackend.onrender.com/history");
+        const response = await fetch("https://brain-server-xuxb.onrender.com/history");
         const data = await response.json();
 
         if (response.ok) {
@@ -68,7 +68,7 @@ const AdminPrediction = () => {
 
   const handleDeletePrediction = async (id) => {
     try {
-      const res = await fetch(`https://brain-tumourbackend.onrender.com/delete-history/${id}`, {
+      const res = await fetch(`https://brain-server-xuxb.onrender.com/delete-history/${id}`, {
         method: "DELETE",
       });
 
